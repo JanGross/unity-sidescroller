8e2935b461cd4b99a17ca6e7e0ed0bb5f290de89
Jan Groß <minzkraut@gmail.com>
2017-03-31 23:54:23 +0200

    Introducing the Helper2D class

---
+++ Assets/Scripts/Helper2D.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Helper2D 
+{
+
+    public static Vector3 getMousePosInWorldSpace ()
+    {
+        Vector3 mousePositionInWorldSpace = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+        mousePositionInWorldSpace.z = 0;
+
+        return mousePositionInWorldSpace;
+    }
+
+}
