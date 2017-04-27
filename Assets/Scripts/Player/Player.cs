829dc9f64aacbe4d149852638a960ee578f07cf1
Jan Groß <minzkraut@gmail.com>
2017-04-27 20:01:13 +0200

    Check if obsatcle is in move direction, broke arm rotation

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -32,8 +32,6 @@ public class Player : MonoBehaviour
             Vector3 armRotation = this.arm.transform.rotation.eulerAngles; 
             int direction = mousePositionInWorldSpace.x < this.arm.transform.position.x ? -1 : 1;
             armRotation.z = Vector3.Angle (this.arm.transform.position, mousePositionInWorldSpace) * direction;
-            Debug.Log (armRotation);
-
             this.arm.transform.rotation = Quaternion.Euler(armRotation);
         }
     }
@@ -63,24 +61,14 @@ public class Player : MonoBehaviour
         {
             movementSpeed = this.playerSettings.airControlSpeed;
         }
-//        if (this.rigidbody.velocity.sqrMagnitude > this.maxVelocity)
-//        {
-//            
-//            float speedDecrease = -(this.rigidbody.velocity.sqrMagnitude - this.maxVelocity);
-//            Vector2 normalizedVelocity = this.rigidbody.velocity.normalized;
-//            this.rigidbody.AddForce (normalizedVelocity * speedDecrease);
-//            Debug.LogWarning ("TOO FAST" + this.rigidbody.velocity.sqrMagnitude);
-//        }
 
         if (Input.GetAxisRaw ("Horizontal") < 0)
         {
-            //this.rigidbody.AddForce (Vector2.left * movementSpeed, ForceMode2D.Impulse);
-            transform.Translate (Vector2.left * Time.deltaTime * movementSpeed);
+            this.moveInDirection (Vector2.left);
         } 
         else if (Input.GetAxisRaw ("Horizontal") > 0)
         {
-            //this.rigidbody.AddForce (Vector2.right * movementSpeed, ForceMode2D.Impulse);
-            transform.Translate (Vector2.right * Time.deltaTime * movementSpeed);    
+            this.moveInDirection (Vector2.right);    
         }
 
         if (Input.GetAxisRaw ("Vertical") > 0 && this.canJump)
@@ -94,11 +82,12 @@ public class Player : MonoBehaviour
     {
         foreach (ContactPoint2D contactPoint in collision.contacts) {
             Debug.DrawRay(new Vector2(contactPoint.point.x, contactPoint.point.y), contactPoint.normal, Color.black);
-            //this.Log ( ""+Vector2.Angle (contactPoint.normal, Vector2.up) );
             if (this.debug)
             {
-                Vector3 point1 = new Vector3 (transform.position.x - this.collider.bounds.extents.x, 
-                                     transform.position.y - (this.collider.bounds.extents.y - this.playerSettings.collisionPadding), 0);
+                Vector3 point1 = new Vector3 (
+                                            transform.position.x - this.collider.bounds.extents.x, 
+                                            transform.position.y - (this.collider.bounds.extents.y - this.playerSettings.collisionPadding),
+                                            0);
                 Debug.DrawRay (point1, Vector3.left, Color.green);
             }
             if (contactPoint.point.y <= (transform.position.y - (this.collider.bounds.extents.y - this.playerSettings.collisionPadding))) 
@@ -113,6 +102,21 @@ public class Player : MonoBehaviour
     void Jump (float strength)
     {
         this.rigidbody.AddForce (Vector2.up * strength, ForceMode2D.Impulse);
+    }
+
+    void moveInDirection(Vector2 direction, float speed = 0)
+    {
+        float moveSpeed = speed != 0 ? speed : this.playerSettings.walkSpeed;
+        Vector2 rayStartPosition = (Vector2)transform.position + (direction.normalized * (this.collider.bounds.extents.x + 0.01f));
+        RaycastHit2D hit = Physics2D.Raycast (rayStartPosition, direction, this.playerSettings.collisionPadding);
+        Debug.DrawLine(rayStartPosition, hit.point, Color.gray);
+
+
+        if (hit.collider == null)
+        {
+            transform.Translate (direction * Time.deltaTime * moveSpeed);
+        }
+        Debug.Log (hit.collider);
     }
 
     void Log (string message)
